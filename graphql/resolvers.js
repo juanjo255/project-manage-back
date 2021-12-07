f6c9c1915b21a4aadca6b86d5c9372435fc4e564
juanjo255 <piconcossio@gmail.com>
2021-12-07 15:03:43 -0500

    definiendo funcionalidades

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,6 +1,7 @@
 import { Users_Controllers } from "../models/usuario/resolvers.js";
 import { Resolvers_Projects } from "../models/proyecto/resolvers.js";
 import { Inscriptions_Resolvers } from "../models/inscripcion/resolvers.js";
+
 const resolvers = [Users_Controllers, Resolvers_Projects, Inscriptions_Resolvers]
 
 export {resolvers}
